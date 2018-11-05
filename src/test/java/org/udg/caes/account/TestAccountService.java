49f016e5c241f3a7d3b06e2e8f1646a5c8f120e2
Maurisss94 <maurobale94@hotmail.es>
2018-11-05 17:38:12 +0100

    Test unitari testTransfer fet.

--- src/test/java/org/udg/caes/account/TestAccountService.java
+++ src/test/java/org/udg/caes/account/TestAccountService.java
@@ -15,6 +15,25 @@ public class TestAccountService {
 
   @Test
   void testTransfer(@Tested AccountService as, @Mocked AccountManager am)  {
-    throw new UnsupportedOperationException("Not implemented yet");
+
+    //Arrange
+    Account acc1 = new Account("user1", 1000);
+    Account acc2 = new Account("user2", 1000);
+    as.setAccountManager(am);
+    new Expectations() {{
+      am.findAccount("user1"); result = acc1;
+      am.findAccount("user2"); result = acc2;
+    }};
+    //Act
+    as.transfer("user1", "user2", 250);
+
+    //Assert
+    assertEquals(750, acc1.getBalance());
+    assertEquals(1250, acc2.getBalance());
+    new Verifications(){{
+      am.updateAccount(acc1); times = 1;
+      am.updateAccount(acc2); times = 1;
+    }};
+
   }
 }
